ad2c7b4cf3969b24df16bfd91989c5feb1c0d84f
Aleksey <example@gmail.com>
2021-10-23 17:20:08 +0300

    MapDispatchToProps

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,19 +1,19 @@
-import React                                                                          from 'react';
+import React      from 'react';
 import {connect}  from 'react-redux';
 import {
-    followAC ,
-    setCurrentPageAC ,
-    setTotalUsersCountAC ,
-    setUsersAC ,
-    toggleIsLoadingAC ,
-    unfollowAC
+    follow ,
+    setCurrentPage ,
+    setTotalUsersCount ,
+    setUsers ,
+    toggleIsLoading ,
+    unfollow
 }                 from '../../redux/users-reducer';
 import * as axios from 'axios';
 import Users      from './Users';
 import Preloader  from "../common/Preloader/Preloader";
 
 
-class UsersContainer extends React.Component{
+class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsLoading(true);
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(
@@ -42,48 +42,26 @@ class UsersContainer extends React.Component{
         return <>
             {this.props.isLoading ? <Preloader/> : null}
             <Users totalUsersCount={this.props.totalUsersCount}
-                      pageSize={this.props.pageSize}
-                      currentPage={this.props.currentPage}
-                      onPageChanged={this.onPageChanged}
-                      users={this.props.users}
-                      follow={this.props.follow}
-                      unfollow={this.props.unfollow}/>
-            </>
+                   pageSize={this.props.pageSize}
+                   currentPage={this.props.currentPage}
+                   onPageChanged={this.onPageChanged}
+                   users={this.props.users}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}/>
+        </>
     }
 }
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
+        users: state.usersPage.users ,
+        pageSize: state.usersPage.pageSize ,
+        totalUsersCount: state.usersPage.totalUsersCount ,
+        currentPage: state.usersPage.currentPage ,
         isLoading: state.usersPage.isLoading
     }
 }
 
-let mapDispatchToProps = (dispatch) => {
-    return {
-        follow: (userId) => {
-            dispatch(followAC(userId));
-        },
-        unfollow: (userId) => {
-            dispatch(unfollowAC(userId));
-        },
-        setUsers: (users) => {
-            dispatch(setUsersAC(users))
-        },
-        setCurrentPage: (pageNumber) => {
-            dispatch(setCurrentPageAC(pageNumber))
-        },
-        setTotalUsersCount: (pageNumber) => {
-            dispatch(setTotalUsersCountAC(pageNumber))
-        },
-        toggleIsLoading: (isLoading) => {
-            dispatch(toggleIsLoadingAC(isLoading))
-        },
-
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
+export default connect(mapStateToProps , {
+    follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsLoading
+})(UsersContainer);
